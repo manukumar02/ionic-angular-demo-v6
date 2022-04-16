a92716cd900750fcc114c94ddd0630eeb03316c3
Manu <manukumar02@users.noreply.github.com>
2022-04-16 21:38:48 +0530

    initial commits

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  dummy = [
+    { name: 'Shyam', email: 'shyamjaiswal@gmail.com' },
+    { name: 'Bob', email: 'bob32@gmail.com' },
+    { name: 'Jai', email: 'jai87@gmail.com' },
+  ];
+  constructor(public actionSheetCtrl: ActionSheetController) {}
 
-  constructor() {}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Action Sheet',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            console.log('Delete clicked');
+          },
+        },
+        {
+          text: 'Share',
+          icon: 'share',
+          handler: () => {
+            console.log('Share clicked');
+          },
+        },
+        {
+          text: 'Play (open modal)',
+          icon: 'caret-forward-circle',
+          handler: () => {
+            console.log('Play clicked');
+          },
+        },
+      ],
+      cssClass: 'custom-class',
+      animated: true,
+      backdropDismiss: true,
+      keyboardClose: false,
+    });
 
+    actionSheet.present();
+  }
 }
